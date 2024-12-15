8ca37f19aa2106ccb747868db96adb10b09b433d
Md Amran Hossen <mdamranhossen77@gmail.com>
2024-12-16 00:04:00 +0600

    parallal route added

--- app/(WithCommonLayout)/(home)/page.tsx
+++ app/(WithCommonLayout)/(home)/page.tsx
@@ -1,12 +1,11 @@
+
 import Landing from "@/components/modules/home/Landing";
-import RecentPost from "@/components/modules/home/RecentPost";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
       <Landing />
-      <RecentPost />
     </>
   );
 };
