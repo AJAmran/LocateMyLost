c369fb923330a06fa11938769febaff8f5bbb5df
Md Amran Hossen <mdamranhossen77@gmail.com>
2024-12-24 23:20:47 +0600

    transtack query basic setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
-
-import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/ui/navbar";
+import { Providers } from "@/lib/Providers";
 
 export const metadata: Metadata = {
   title: {
